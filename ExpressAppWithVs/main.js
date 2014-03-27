f95ffb37f56a0705f76e53a37eae479f286b6562
Minh Tam <minhtam.seta@gmail.com>
2014-03-27 21:40:35 +0700

    Filter data

--- ExpressAppWithVs/main.js
+++ ExpressAppWithVs/main.js
@@ -1,14 +1,26 @@
 ﻿(function(){
 	var presidents = [
-	                  {
-	                	  name: "Geogre Washington"
-	                  },
-	                  {
-	                	  name: "Abramham Lincoln"
-	                  },
-	                  {
-	                	  name: "Franklin Delano Roosevelt"
-	                  }];
+					{
+						name: "Geogre Washington",
+						country: "US"                                                                                       
+					},
+					{
+						name: "Abramham Lincoln",
+						country: "US"
+					},
+					{
+						name: "Franklin Delano Roosevelt",
+						country: "US"
+					},
+					{
+						name: "Vladimir Ilyich Lenin",
+						country: "Russia"
+					},
+					{
+						name: "Vladimir Vladimirovich Putin",
+						country: "Russia"
+					}
+					];
 	initialize();
 	//testConnect();
 	
@@ -48,7 +60,11 @@
 	       //configuration for public folder
 	       //app.use('')
 	       //----end
+           app.use(express.urlencoded());
+           //app.use(express.bodyParser());
+           app.use(express.favicon(path.join(__dirname, 'favicon.ico')));
 	       app.use('/content', express.static(path.join(__dirname, '/public'), { maxAge: oneDay}));
+           app.use('/uploadFiles', express.static(path.join(__dirname, '/filesUploaded')));
 	       app.use(app.router);
 	       app.use(function(req, res){
 		    	res.status(404).render('error404.ejs', {url: req.url});
@@ -67,6 +83,12 @@
 	    app.get('/presidents', function(req, res){
 	    	res.render('list', { presidents: presidents });
 	    });
+        app.get('/presidents/:country', function(req, res){
+        	var data = presidents.filter(function(president){
+    			return president.country == req.params.country;
+        	});
+        	res.render('list', { presidents: data });
+        });
 	    app.listen(app.get('port'));	    
 	    console.log(new Date());
   }})();
